edd3f292348b8bd96f5bf4daad07b2719c1a61f8
Jason Shiffler <jasonshiffler@yahoo.com>
2020-04-29 07:47:06 -0500

    Initial Commit

---
+++ src/main/java/com/shiffler/springsecurityclass/dao/Student.java
@@ -0,0 +1,14 @@
+package com.shiffler.springsecurityclass.dao;
+
+import lombok.Data;
+
+@Data
+public class Student {
+
+    private final Integer studentId;
+    private final String studentName;
+
+
+
+
+}
---
+++ src/test/java/com/shiffler/springsecurityclass/SpringSecurityClassApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shiffler.springsecurityclass;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityClassApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
